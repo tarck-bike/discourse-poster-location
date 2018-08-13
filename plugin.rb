5b394ad3d58afc3065708cc2adde31aa982b64b0
Zach Nedwich <znedwich@gmail.com>
2018-08-13 19:56:57 +1000

    rendering issue

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
 # name: discourse-poster-location
 # about: Show User's location in posts
-# version: 1.0.8
+# version: 1.0.9
 # author: Zach Nedwich <zach@znedw.com>
 # url: https://github.com/zachnedwich/discourse-poster-location
 
