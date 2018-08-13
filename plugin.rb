1038539499214c7e3fdf00aa9905b171636ee7b6
Zach Nedwich <znedwich@gmail.com>
2018-08-13 19:03:07 +1000

    Extra field for location

--- plugin.rb
+++ plugin.rb
@@ -8,8 +8,33 @@ enabled_site_setting :posterlocation_enabled
 
 PLUGIN_NAME = "discourse-posterlocation"
 
+DiscoursePluginRegistry.serialized_current_user_fields << "posterlocation"
 after_initialize do
-  
+  public_user_custom_fields_setting = SiteSetting.public_user_custom_fields
+  if public_user_custom_fields_setting.empty?
+    SiteSetting.set("public_user_custom_fields", "posterlocation")
+  elsif public_user_custom_fields_setting !~ /posterlocation/
+    SiteSetting.set(
+      "public_user_custom_fields",
+      [SiteSetting.public_user_custom_fields, "posterlocation"].join("|")
+    )
+  end
+
+  User.register_custom_field_type('posterlocation', :text)
+
+  if SiteSetting.posterlocation_enabled then
+    add_to_serializer(:post, :user_signature, false) {
+      object.user.custom_fields['posterlocation']
+    }
+
+    add_to_serializer(:user, :custom_fields, false) {
+      if object.custom_fields == nil then
+        {}
+      else
+        object.custom_fields
+      end
+    }
+  end
 end
 
 # not needed but if we want to style Location, do it here
