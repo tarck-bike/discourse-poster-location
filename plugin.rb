d84a50a3e6b5d8cc724cda728cebb5dd55c7a701
Zach Nedwich <zach@znedw.com>
2023-09-15 19:24:55 +1000

    fix deprecated_respect_plugin_enabled

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
 # name: discourse-poster-location
 # about: Show User's location in posts
-# version: 1.0.16
+# version: 1.0.17
 # author: Zach Nedwich <zach@znedw.com>
 # url: https://github.com/tarck-bike/discourse-poster-location
 
@@ -25,11 +25,11 @@ after_initialize do
   register_editable_user_custom_field :posterlocation if defined? register_editable_user_custom_field
 
   if SiteSetting.posterlocation_enabled then
-    add_to_serializer(:post, :user_signature, false) {
+    add_to_serializer(:post, :user_signatures) {
       object.user.custom_fields['posterlocation']
     }
 
-    add_to_serializer(:user, :custom_fields, false) {
+    add_to_serializer(:user, :custom_fields) {
       if object.custom_fields == nil then
         {}
       else
